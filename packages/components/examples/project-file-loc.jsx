9eb942b8aead5a07df741ff3e3e3c60eb400c024
Heidi Kasemir <heidi.kasemir@gmail.com>
2018-11-19 16:20:34 -0800

    initial file restructure

---
+++ packages/components/examples/project-file-loc.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { signOut } from 'utils';
+import warningImg from 'assets/images';
+import { Loc } from '../src';
+
+export function NotAuthorized() {
+  return (
+    <div className="container">
+      <Loc.H1 l10nId="NotAuthorized_oopsMessage">
+        {/* here is a comment */}
+        Oops! looks like you are not authorized to use this app.
+      </Loc.H1>
+      <Loc.Img l10nId="NotAuthorized_warningImg" alt="warning sign" src={warningImg} />
+      <Loc.P
+        l10nId="NotAuthorized_possibleActions"
+        l10nJsx={{
+          link: <Link to="/" />,
+          a: <a href="https://www.udacity.com" />,
+          button: <button onClick={signOut} />
+        }}
+      >
+        {'<link>Try again</link>, go <a>Home</a> or <button>Logout</button>'}
+      </Loc.P>
+    </div>
+  );
+}
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { Loc, makeLocalizedElement, augmentLoc, pseudolocalize } from './components';
-
-export default {
-  Loc, makeLocalizedElement, augmentLoc, pseudolocalize
-};
