7b73870499a71fc316779c012c46a318c5fcf101
Heidi Kasemir <heidi.kasemir@gmail.com>
2018-12-10 17:01:04 -0800

    move constants out of shared folder

---
+++ packages/extraction/src/constants.js
@@ -0,0 +1,14 @@
+export const AST_NODE_TYPES = {
+  JSXElement: 'JSXElement',
+  JSXText: 'JSXText',
+  JSXExpressionContainer: 'JSXExpressionContainer',
+  JSXEmptyExpression: 'JSXEmptyExpression',
+  StringLiteral: 'StringLiteral'
+};
+
+export const FLUENT_ATTRS = {
+  attrs: 'attrs',
+  l10nId: 'l10nId',
+  l10nVars: 'l10nVars',
+  l10nJSX: 'l10nJSX'
+};
