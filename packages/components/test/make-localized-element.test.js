a71d3453d5d1910670ade23acb45baf35229e9b8
Art Gillespie <art@udacity.com>
2019-01-14 11:02:53 -0800

    Add jest to ./components package

--- packages/components/test/make-localized-element.test.js
+++ packages/components/test/make-localized-element.test.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Localized } from 'fluent-react/compat';
-import { assert, expect } from 'chai';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { formatVars, makeLocalizedElement } from '../src/make-localized-element';
@@ -11,7 +9,7 @@ describe('formatVars', () => {
   it('formats the l10nVars variables for spreading into the Localized', () => {
     const l10nVars = { name: 'Alice', number: 123 };
     const expectedOutput = { $name: 'Alice', $number: 123 };
-    expect(formatVars(l10nVars)).to.deep.equal(expectedOutput);
+    expect(formatVars(l10nVars)).toEqual(expectedOutput);
   });
 });
 
@@ -19,47 +17,27 @@ describe('makeLocalizedElement', () => {
   it('Makes an element wrapped in a Localized component', () => {
     const Component = makeLocalizedElement('h1');
     const wrapper = shallow(<Component l10nId="test" />);
-    const expectedOutput = (
-      <Localized id="test">
-        <h1 />
-      </Localized>
-    );
-    assert(wrapper.contains(expectedOutput));
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('Applies the attrs', () => {
     const Component = makeLocalizedElement('img', { alt: true });
     const wrapper = shallow(<Component l10nId="test" />);
-    const expectedOutput = (
-      <Localized id="test" attrs={{ alt: true }}>
-        <img />
-      </Localized>
-    );
-    assert(wrapper.contains(expectedOutput));
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('Spreads in the l10nVars', () => {
     const Component = makeLocalizedElement('h1');
     const l10nVars = { name: 'Alice', number: 123 };
     const wrapper = shallow(<Component l10nId="test" l10nVars={l10nVars} />);
-    const expectedOutput = (
-      <Localized id="test" $name="Alice" $number={123}>
-        <h1 />
-      </Localized>
-    );
-    assert(wrapper.contains(expectedOutput));
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('Spreads in the l10nJsx', () => {
     const Component = makeLocalizedElement('h1');
     const l10nJsx = { link: <a href="https://example.com" /> };
     const wrapper = shallow(<Component l10nId="test" l10nJsx={l10nJsx} />);
-    const expectedOutput = (
-      <Localized id="test" link={<a href="https://example.com" />}>
-        <h1 />
-      </Localized>
-    );
-    assert(wrapper.contains(expectedOutput));
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('Applies the other props to the child', () => {
@@ -70,11 +48,6 @@ describe('makeLocalizedElement', () => {
       </Component>
     );
     const wrapper = shallow(input);
-    const expectedOutput = (
-      <Localized id="test">
-        <h1 className="title_header">child text</h1>
-      </Localized>
-    );
-    assert(wrapper.contains(expectedOutput));
+    expect(wrapper).toMatchSnapshot();
   });
 });
